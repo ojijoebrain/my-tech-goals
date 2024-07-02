e6ea7149d48f62cd84a42b0825075378c89fa91b
Oji joseph Oji <ojijoebrain73@gmail.com>
2024-07-02 22:24:21 +0100

    project task

---
+++ Script.js
@@ -0,0 +1,14 @@
+
+function updateTime() {
+    const now = new Date();
+    const utcTime = now.toUTCString().slice(-12, -4);
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayOfWeek = days[now.getUTCDay()];
+
+    document.getElementById('currentTimeUTC').textContent = utcTime;
+    document.getElementById('currentDay').textContent = dayOfWeek;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
